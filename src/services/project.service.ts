06c13b6d74d2809612854ac44a3df462fb12bf52
Sahan Chamara <sahanchamara456@gmail.com>
2025-04-30 22:42:16 +0530

    get project by workpace id

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -5,107 +5,123 @@ import { NotFoundException } from "../utils/appError";
 
 // Create Project
 export const createProjectService = async (
-    userId: string,
-    workspaceId: string,
-    body: {
-        emoji?: string;
-        name: string;
-        description?: string;
-    }
+  userId: string,
+  workspaceId: string,
+  body: {
+    emoji?: string;
+    name: string;
+    description?: string;
+  }
 ) => {
-    const project = new ProjectModel({
-        ...(body.emoji && { emoji: body.emoji }),
-        name: body.name,
-        description: body.description,
-        workspace: workspaceId,
-        createdBy: userId,
-    });
+  const project = new ProjectModel({
+    ...(body.emoji && { emoji: body.emoji }),
+    name: body.name,
+    description: body.description,
+    workspace: workspaceId,
+    createdBy: userId,
+  });
 
-    await project.save();
+  await project.save();
 
-    return { project };
+  return { project };
 };
 
 // Update Project
 export const updateProjectService = async (
-    workspaceId: string,
-    projectId: string,
-    body: {
-        emoji?: string;
-        name: string;
-        description?: string;
-    }
+  workspaceId: string,
+  projectId: string,
+  body: {
+    emoji?: string;
+    name: string;
+    description?: string;
+  }
 ) => {
-    const { name, emoji, description } = body;
+  const { name, emoji, description } = body;
 
-    const project = await ProjectModel.findOne({
-        _id: projectId,
-        workspace: workspaceId,
-    });
+  const project = await ProjectModel.findOne({
+    _id: projectId,
+    workspace: workspaceId,
+  });
 
-    if (!project) {
-        throw new NotFoundException(
-            "Project not found or does not belong to the specified workspace"
-        );
-    }
+  if (!project) {
+    throw new NotFoundException(
+      "Project not found or does not belong to the specified workspace"
+    );
+  }
 
-    if (emoji) project.emoji = emoji;
-    if (name) project.name = name;
-    if (description) project.description = description;
+  if (emoji) project.emoji = emoji;
+  if (name) project.name = name;
+  if (description) project.description = description;
 
-    await project.save();
+  await project.save();
 
-    return { project };
+  return { project };
 };
 
 // delete project
 export const deleteProjectService = async (
-    workspaceId: string,
-    projectId: string
-  ) => {
-    const project = await ProjectModel.findOne({
-      _id: projectId,
-      workspace: workspaceId,
-    });
-  
-    if (!project) {
-      throw new NotFoundException(
-        "Project not found or does not belong to the specified workspace"
-      );
-    }
-  
-    await project.deleteOne();
-  
-    await TaskModel.deleteMany({
-      project: project._id,
-    });
-  
-    return project;
-  };
+  workspaceId: string,
+  projectId: string
+) => {
+  const project = await ProjectModel.findOne({
+    _id: projectId,
+    workspace: workspaceId,
+  });
 
-  // get project in workspace
-  export const getProjectsInWorkspaceService = async (
-    workspaceId: string,
-    pageSize: number,
-    pageNumber: number
-  ) => {
-    // Step 1: Find all projects in the workspace
-  
-    const totalCount = await ProjectModel.countDocuments({
-      workspace: workspaceId,
-    });
-  
-    const skip = (pageNumber - 1) * pageSize;
-  
-    const projects = await ProjectModel.find({
-      workspace: workspaceId,
-    })
-      .skip(skip)
-      .limit(pageSize)
-      .populate("createdBy", "_id name profilePicture -password")
-      .sort({ createdAt: -1 });
-  
-    const totalPages = Math.ceil(totalCount / pageSize);
-  
-    return { projects, totalCount, totalPages, skip };
-  };
+  if (!project) {
+    throw new NotFoundException(
+      "Project not found or does not belong to the specified workspace"
+    );
+  }
+
+  await project.deleteOne();
+
+  await TaskModel.deleteMany({
+    project: project._id,
+  });
+
+  return project;
+};
+
+// get project in workspace
+export const getProjectsInWorkspaceService = async (
+  workspaceId: string,
+  pageSize: number,
+  pageNumber: number
+) => {
+  // Step 1: Find all projects in the workspace
+
+  const totalCount = await ProjectModel.countDocuments({
+    workspace: workspaceId,
+  });
+
+  const skip = (pageNumber - 1) * pageSize;
+
+  const projects = await ProjectModel.find({
+    workspace: workspaceId,
+  })
+    .skip(skip)
+    .limit(pageSize)
+    .populate("createdBy", "_id name profilePicture -password")
+    .sort({ createdAt: -1 });
+
+  const totalPages = Math.ceil(totalCount / pageSize);
+
+  return { projects, totalCount, totalPages, skip };
+};
+
+export const getProjectByWorkspaceIdService = async (
+  workspaceId: StaticRange,
+  projectId: string
+) => {
+  const project = await ProjectModel.findOne({
+    _id: projectId,
+    workspace: workspaceId,
+  }).select("_id emoji name description");
+
+  if (!project) {
+    throw new NotFoundException("Project not Found or does not blong the specified workspace");
+  }
+
+  return { project };
+}
